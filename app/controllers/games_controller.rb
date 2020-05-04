3b2a583488e4e0ad8bca7d9977bbebf60b15b64e
lucassherwin <lksherwin@gmail.com>
2020-05-04 16:43:07 -0400

    controllers and models

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,2 +1,10 @@
 class GamesController < ApplicationController
+    def index
+        games = Games.all
+        render json: games
+    end
+
+    def show
+        game = Game.find_by(id: params[:id])
+    end
 end
