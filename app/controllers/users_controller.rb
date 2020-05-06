21344ec0584c5bf5a3737b02ce4cd6249bbee0b6
lucassherwin <lksherwin@gmail.com>
2020-05-06 10:21:01 -0400

    changes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,7 @@ class UsersController < ApplicationController
     end
 
     def create
-        user = User.new(user_params)
+        user = User.new(params)
         user.save
     end
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,10 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Category.destroy_all
+Question.destroy_all
 
+cat1 = Category.create!(name: "doesnt matter")
+puts cat1.name
 
-# cat1 = Category.create(name: "doesnt matter")
-
-# q1 = Question.create(category_id: cat1, question: "test question 1", correct_answer: "correct", wrong_ans_1: "wrong 1", wrong_ans_2: "wrong 2", wrong_ans_3: "wrong 3")
-# q2 = Question.create(category_id: cat1, question: "test question 2", correct_answer: "correct", wrong_ans_1: "wrong 1", wrong_ans_2: "wrong 2", wrong_ans_3: "wrong 3")
-# q3 = Question.create(category_id: cat1, question: "test question 3", correct_answer: "correct", wrong_ans_1: "wrong 1", wrong_ans_2: "wrong 2", wrong_ans_3: "wrong 3")
+q1 = Question.create!(category_id: cat1.id, question: "test question 1", correct_answer: "correct", wrong_ans_1: "wrong 1", wrong_ans_2: "wrong 2", wrong_ans_3: "wrong 3")
+q2 = Question.create!(category_id: cat1.id, question: "test question 2", correct_answer: "correct", wrong_ans_1: "wrong 1", wrong_ans_2: "wrong 2", wrong_ans_3: "wrong 3")
+q3 = Question.create!(category_id: cat1.id, question: "test question 3", correct_answer: "correct", wrong_ans_1: "wrong 1", wrong_ans_2: "wrong 2", wrong_ans_3: "wrong 3")
